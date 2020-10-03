b69612b12b27c474d1e9da3118d3024b5ce6dbfd
Den Moskvitin <den-lb@ukr.net>
2020-10-03 12:04:09 +0300

    fix: add modern solution

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 exports.min = function min (array) {
     if (array && array.length > 0) {
         return array.sort((a, b) => a - b )[0];
+        // return Math.min(...array);
     }
   return 0;
 }
@@ -9,13 +10,14 @@ exports.min = function min (array) {
 exports.max = function max (array) {
     if (array && array.length > 0) {
         return array.sort((a, b) => b - a )[0];
+        // return Math.max(...array);
     }
   return 0;
 }
 
 exports.avg = function avg (array) {
     if (array && array.length > 0) {
-        const sumVal = array.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+        const sumVal = array.reduce((acc, val) => acc + val, 0);
         return sumVal / array.length;
     }
   return 0;
